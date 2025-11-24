4aabddcfde4d8c96aea3b51fb63a84b55d1d47a6
Fatemeh-Gharche <fatq905@gmail.com>
2025-11-25 02:01:24 +0330

    Add and test S/G positions

--- map_loader.py
+++ map_loader.py
@@ -8,3 +8,21 @@ def load_map():
         grid.append(list(line))
 
     return rows, cols, grid
+
+
+
+def find_positions(grid):
+    start = None
+    goals = []
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            cell = grid[r][c]
+
+            if cell == 'S':
+                start = (r, c)
+
+            if cell == 'G':
+                goals.append((r, c))
+
+    return start, goals
--- scenario1_uninformed.py
+++ scenario1_uninformed.py
@@ -1,9 +1,15 @@
-from map_loader import load_map
+from map_loader import load_map, find_positions
 
 rows, cols, grid = load_map()
+
+start, goals = find_positions(grid)
 
 print("Rows:", rows)
 print("Cols:", cols)
 print("Map:")
 for row in grid:
     print(row)
+
+print("Start position:", start)
+print("Victims:", goals) 
+
