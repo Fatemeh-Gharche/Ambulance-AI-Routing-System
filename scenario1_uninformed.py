20eaa2968a2fa0a9454d85c51ec7741bc8017331
Fatemeh-Gharche <fatq905@gmail.com>
2025-11-25 03:35:35 +0330

    BFS queue and visited

--- scenario1_uninformed.py
+++ scenario1_uninformed.py
@@ -1,5 +1,6 @@
 from map_loader import load_map, find_positions
 from search_utils import create_initial_state, MOVES, get_cost
+from collections import deque
 
 rows, cols, grid = load_map()
 
@@ -20,3 +21,20 @@ print(state)
 
 print("\nMoves:")
 print(MOVES)
+
+
+def initialize_bfs(start):
+    queue = deque()
+    visited = set()
+
+    start_state = create_initial_state(start)
+    queue.append(start_state)
+
+    visited.add((start_state[0], start_state[1]))  
+
+    return queue, visited
+
+
+queue, visited = initialize_bfs(start)
+print("Initial queue:", queue)
+print("Visited set:", visited)
