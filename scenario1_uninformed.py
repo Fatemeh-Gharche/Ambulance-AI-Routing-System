811eb1226cca96aed3ca7e54650d8dd6f24019b8
n.boghrati <n.boghrati@partsoftware.com>
2025-11-26 18:54:55 +0330

    Refactor to OOP style

--- scenario1_uninformed.py
+++ scenario1_uninformed.py
@@ -1,70 +1,112 @@
-from map_loader import load_map, find_positions
-from search_utils import create_initial_state, MOVES, get_step_cost
+from map_loader import MapLoader
+from search_utils import SearchUtils, State
 import heapq
-
-rows, cols, grid = load_map()
-start, goals = find_positions(grid)
+import itertools
 
 
-def ucs(grid, start, goals):
-    visited = dict()
-    start_state = create_initial_state(start)
-    queue = []
-    heapq.heappush(queue, (0, start_state))
+class UCS:
+    def __init__(self, grid, start, goals):
+        self.grid = grid
+        self.start = start
+        self.goals = goals
+        self.utils = SearchUtils()
+        self.counter = itertools.count()
 
-    expanded = 0
+    def search(self):
+        visited = {}
 
-    while queue:
-        cost_so_far, (current_pos, visited_goals, path_so_far, _) = heapq.heappop(queue)
-        expanded += 1
+        start_state = State(
+            position=self.start,
+            visited_goals=(),
+            path=[],
+            cost=0
+        )
 
-        if set(visited_goals) == set(goals):
-            return path_so_far, cost_so_far, expanded
+        queue = []
+        heapq.heappush(queue, (0, next(self.counter), start_state))
+        expanded = 0
 
-        time_mod = cost_so_far % 20
-        key = (current_pos, tuple(visited_goals), time_mod)
+        while queue:
+            cost_so_far, _, state = heapq.heappop(queue)
 
-        prev_best = visited.get(key)
-        if prev_best is not None and cost_so_far >= prev_best:
-            continue
-        visited[key] = cost_so_far
+            current_pos = state.position
+            visited_goals = state.visited_goals
+            path_so_far = state.path
+            expanded += 1
 
-        for move, (dr, dc) in MOVES.items():
-            nr, nc = current_pos[0] + dr, current_pos[1] + dc
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                cell = grid[nr][nc]
-                new_path = path_so_far.copy()
-                current_time = cost_so_far
+            if set(visited_goals) == set(self.goals):
+                return path_so_far, cost_so_far, expanded
 
-                if move == 'STAY':
-                    new_path.append('STAY')
-                    new_cost = current_time + 1
-                elif cell == 'L':
-                    while current_time % 20 >= 10:  
+            time_mod = cost_so_far % 20
+            key = (current_pos, visited_goals, time_mod)
+
+            prev_cost = visited.get(key)
+            if prev_cost is not None and cost_so_far >= prev_cost:
+                continue
+
+            visited[key] = cost_so_far
+
+            for move, (dr, dc) in self.utils.MOVES.items():
+                nr, nc = current_pos[0] + dr, current_pos[1] + dc
+
+                if 0 <= nr < len(self.grid) and 0 <= nc < len(self.grid[0]):
+                    cell = self.grid[nr][nc]
+
+                    new_path = path_so_far.copy()
+                    current_time = cost_so_far
+
+                    if move == 'STAY':
                         new_path.append('STAY')
+                        new_cost = current_time + 1
+
+                    elif cell == 'L':
+                        while current_time % 20 >= 10:
+                            new_path.append('STAY')
+                            current_time += 1
+
+                        new_path.append(move)
                         current_time += 1
-                    new_path.append(move)
-                    current_time += 1
-                    new_cost = current_time
-                else:
-                    step = get_step_cost(cell, current_time)
-                    new_path.append(move)
-                    new_cost = current_time + step
+                        new_cost = current_time
 
-                new_visited = list(visited_goals)
-                if cell == 'G' and (nr, nc) not in visited_goals:
-                    new_visited.append((nr, nc))
+                    else:
+                        step_cost = self.utils.step_cost(cell, current_time)
+                        new_path.append(move)
+                        new_cost = current_time + step_cost
 
-                new_state = ((nr, nc), tuple(new_visited), new_path, new_cost)
-                heapq.heappush(queue, (new_cost, new_state))
+                    new_visited = list(visited_goals)
+                    if cell == 'G' and (nr, nc) not in visited_goals:
+                        new_visited.append((nr, nc))
 
-    return None, None, expanded
+                    new_state = State(
+                        position=(nr, nc),
+                        visited_goals=tuple(new_visited),
+                        path=new_path,
+                        cost=new_cost
+                    )
 
-path, cost, expanded = ucs(grid, start, goals)
+                    heapq.heappush(
+                        queue,
+                        (new_cost, next(self.counter), new_state)
+                    )
 
-if path is None:
-    print("No solution found.")
-else:
-    print(f"\nCost: {cost} min")
-    print("Actions:", path)
-    print("Expanded States:", expanded)
+        return None, None, expanded
+
+def main():
+    loader = MapLoader()
+    rows, cols, grid = loader.load_map()
+
+    start, goals = loader.find_positions(grid)
+
+    ucs = UCS(grid, start, goals)
+    path, cost, expanded = ucs.search()
+
+    if path is None:
+        print("No solution found.")
+    else:
+        print(f"\nCost: {cost} min")
+        print("Actions:", path)
+        print("Expanded States:", expanded)
+
+
+if __name__ == "__main__":
+    main()
